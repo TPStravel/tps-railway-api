99dd4722fbd2c77caae646ec910a2e8a787fa77d
Richard Ji <jualigi@gmail.com>
2025-07-03 19:44:06 -0300

    Adicionar preflight CORS para canalvivo.org

--- server.js
+++ server.js
@@ -146,11 +146,15 @@ app.use(helmet({
 
 app.use(compression());
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? 
-    ['https://canalvivo.org', 'https://app.canalvivo.org', 'https://tps-railway-api-production.up.railway.app'] : 
-    ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    origin: [
+        'https://canalvivo.org',
+        'https://www.canalvivo.org',
+        'https://tps-railway-api-production.up.railway.app',
+        'http://localhost:3000'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key']
 }));
 
 const limiter = rateLimit({
@@ -161,6 +165,7 @@ const limiter = rateLimit({
     retryAfter: '15 minutes'
   }
 });
+app.options('*', cors())
 app.use('/api/', limiter);
 
 app.use(express.json({ limit: '10mb' }));
